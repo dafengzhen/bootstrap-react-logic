8f8f4eb0bb4cdbad9ce3b1ce0cfe22f5126def14
dafengzhen <dafengzhen@aliyun.com>
2024-09-26 14:40:49 +0800

    Update button

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -2,13 +2,13 @@ import type { Props } from '@lib/button/types.ts';
 import clsx from 'clsx';
 
 export default function Button(props: Props) {
-  const { className, variant = 'primary', as } = props;
+  const { className, variant, as } = props;
 
   const Component = as || 'button';
 
   return (
     <Component
-      className={clsx('btn', `btn-${variant}`, className)}
+      className={clsx('btn', variant && `btn-${variant}`, className)}
       role="button"
     >
       btn
