f9a36484cb232ce500512718a73a89e43410d351
dafengzhen <dafengzhen@aliyun.com>
2024-10-13 01:22:16 +0800

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bootstrap-react-logic/',
   plugins: [
     react(),
     visualizer({
