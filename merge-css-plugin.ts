ca9686181d8f9ace224bf0025fe29493773a099a
dafengzhen <dafengzhen@aliyun.com>
2024-10-14 22:23:20 +0800

    perf: optimize style construction output

--- merge-css-plugin.ts
+++ merge-css-plugin.ts
@@ -1,19 +1,22 @@
 import type { Plugin } from 'vite';
 import type { OutputAsset } from 'rollup';
+import postcss from 'postcss';
+import cssnano from 'cssnano';
 
 const mergeCssPlugin: Plugin = {
   name: 'merge-css',
-  generateBundle(_, bundle) {
+  async generateBundle(_, bundle) {
     const cssFiles = Object.values(bundle).filter(
       (file): file is OutputAsset => file.type === 'asset' && file.fileName.endsWith('.css'),
     );
 
     if (cssFiles.length > 1) {
-      const mergedCss = cssFiles.map((file) => file.source).join('\n');
+      const mergedCss = cssFiles.map((file) => file.source).join('');
+      const result = await postcss([cssnano({ preset: 'default' })]).process(mergedCss);
       this.emitFile({
         type: 'asset',
         fileName: 'bootstrap-react-logic.css',
-        source: mergedCss,
+        source: result.css,
       });
     }
   },
