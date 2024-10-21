790841d56091056f7c5e31a619e5230fb8a93f54
dafengzhen <dafengzhen@aliyun.com>
2024-10-21 23:51:32 +0800

    chore: generate a sourcemap for 'bootstrap-react-logic.css

--- merge-css-plugin.ts
+++ merge-css-plugin.ts
@@ -1,9 +1,10 @@
 import { normalizePath, type Plugin } from 'vite';
-import type { OutputAsset, SourceMap } from 'rollup';
+import type { OutputAsset, PluginContext, SourceMap } from 'rollup';
 import postcssLoadConfig from 'postcss-load-config';
 import postcss from 'postcss';
 import cssnano from 'cssnano';
 import path from 'path';
+import { SourceMapConsumer, SourceMapGenerator } from 'source-map';
 
 const mergeCssPlugin = (): Plugin => {
   const combinedSourcemap = new Map<string, SourceMap>();
@@ -22,38 +23,103 @@ const mergeCssPlugin = (): Plugin => {
       );
 
       if (cssFiles.length > 1) {
-        const mergedCss = cssFiles.map((file) => file.source).join('');
-        const { plugins, options } = await postcssLoadConfig();
-        const result = await postcss([...plugins, cssnano({ preset: 'default' })]).process(mergedCss, options);
+        const mergedCss = mergeCssSources(cssFiles);
+        const result = await processCssWithPostcss(mergedCss);
+        result.css += `\n/*# sourceMappingURL=bootstrap-react-logic.css.map */`;
 
+        const mergedMap = await mergeSourceMaps(combinedSourcemap);
         this.emitFile({
           type: 'asset',
           fileName: 'bootstrap-react-logic.css',
           source: result.css,
         });
+        this.emitFile({
+          type: 'asset',
+          fileName: 'bootstrap-react-logic.css.map',
+          source: mergedMap.toString(),
+        });
 
-        for (const [filePath, sourcemap] of combinedSourcemap.entries()) {
-          const outputMapPath = getOutputMapPath(filePath);
-          const outputDir = path.dirname(outputMapPath);
-
-          updateSourcemapSources(sourcemap, outputDir);
-
-          this.emitFile({
-            type: 'asset',
-            fileName: outputMapPath,
-            originalFileName: filePath,
-            source: sourcemap.toString(),
-          });
-
-          const cssFile = cssFiles.find((file) => file.fileName === normalizePath(getCssFileName(outputMapPath)));
-          if (cssFile) {
-            const sourceMappingURL = `/*# sourceMappingURL=${path.basename(outputMapPath)} */`;
-            cssFile.source += sourceMappingURL;
-          }
-        }
+        emitUpdatedSourceMaps(cssFiles, combinedSourcemap, this);
       }
     },
   };
+};
+
+const normalizeRelativePath = (from: string, to: string | undefined): string => {
+  return normalizePath(path.relative(from, to || ''));
+};
+
+const mergeCssSources = (cssFiles: OutputAsset[]): string => {
+  return cssFiles.map((file) => file.source).join('');
+};
+
+const processCssWithPostcss = async (css: string) => {
+  const { plugins, options } = await postcssLoadConfig();
+  return postcss([...plugins, cssnano({ preset: 'default' })]).process(css, options);
+};
+
+const mergeSourceMaps = async (combinedSourcemap: Map<string, SourceMap>) => {
+  const mergedMap = new SourceMapGenerator({ file: 'bootstrap-react-logic.css' });
+  const sourcesContentMap = new Map<string, string>();
+
+  for (const sourcemap of combinedSourcemap.values()) {
+    const consumer = await new SourceMapConsumer(sourcemap);
+
+    consumer.sources.forEach((source) => {
+      const content = consumer.sourceContentFor(source);
+      if (content) {
+        sourcesContentMap.set(normalizeRelativePath(process.cwd(), source), content);
+      }
+    });
+
+    consumer.eachMapping((mapping) => {
+      mergedMap.addMapping({
+        generated: {
+          line: mapping.generatedLine,
+          column: mapping.generatedColumn,
+        },
+        source: normalizeRelativePath(process.cwd(), mapping.source),
+        original: {
+          line: mapping.originalLine,
+          column: mapping.originalColumn,
+        },
+        name: mapping.name,
+      });
+    });
+
+    consumer.destroy();
+  }
+
+  sourcesContentMap.forEach((content, source) => {
+    mergedMap.setSourceContent(source, content);
+  });
+
+  return mergedMap;
+};
+
+const emitUpdatedSourceMaps = (
+  cssFiles: OutputAsset[],
+  combinedSourcemap: Map<string, SourceMap>,
+  pluginContext: PluginContext,
+) => {
+  for (const [filePath, sourcemap] of combinedSourcemap.entries()) {
+    const outputMapPath = getOutputMapPath(filePath);
+    const outputDir = path.dirname(outputMapPath);
+
+    updateSourcemapSources(sourcemap, outputDir);
+
+    pluginContext.emitFile({
+      type: 'asset',
+      fileName: outputMapPath,
+      originalFileName: filePath,
+      source: sourcemap.toString(),
+    });
+
+    const cssFile = cssFiles.find((file) => file.fileName === normalizePath(getCssFileName(outputMapPath)));
+    if (cssFile) {
+      cssFile.source += `/*# sourceMappingURL=${path.basename(outputMapPath)} */`;
+    }
+  }
 };
 
 const getOutputMapPath = (filePath: string): string => {
@@ -68,7 +134,7 @@ const getCssFileName = (mapFileName: string): string => {
 
 const updateSourcemapSources = (sourcemap: SourceMap, outputDir: string) => {
   if (sourcemap.sources) {
-    sourcemap.sources = sourcemap.sources.map((sourcePath) => normalizePath(path.relative(outputDir, sourcePath)));
+    sourcemap.sources = sourcemap.sources.map((sourcePath) => normalizeRelativePath(outputDir, sourcePath));
   }
 };
 
