8f43654c2c3048611f2560b4acee73fde7de5143
dafengzhen <dafengzhen@aliyun.com>
2024-11-30 12:27:32 +0800

    docs: update badge

--- src/pages/badge-page.tsx
+++ src/pages/badge-page.tsx
@@ -1,11 +1,12 @@
 import About from '@components/about.tsx';
 import Example from '@components/example.tsx';
 import PropsIndicator from '@components/props-indicator.tsx';
+import TypesIndicator from '@components/types-indicator.tsx';
 import { Badge } from '@lib/badge';
 import { transformCodeObj } from '@src/tools';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigation } from 'react-router-dom';
+import { Link, useNavigation } from 'react-router-dom';
 
 const codes = transformCodeObj(
   import.meta.glob(['../assets/codes/badge/*.md', '../assets/codes/common/*.md'], {
@@ -146,17 +147,12 @@ export default function BadgePage() {
             default: '',
             desc: tBadgeComponentProps('badge.desc.variant'),
             type: (
-              <div className="d-flex flex-column">
-                {['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark', 'link'].map(
-                  (item) => {
-                    return (
-                      <div key={item}>
-                        <span className="badge text-bg-secondary">{item}</span>
-                      </div>
-                    );
-                  },
-                )}
-              </div>
+              <Link to="#badgeComponentTypes">
+                <span className="badge text-bg-secondary d-inline">
+                  Variant
+                  <i className="bi bi-link ms-1"></i>
+                </span>
+              </Link>
             ),
           },
           {
@@ -170,16 +166,99 @@ export default function BadgePage() {
             default: '',
             desc: tBadgeComponentProps('badge.desc.rounded'),
             type: (
-              <>
-                <span className="badge text-bg-secondary">xs | sm | md | lg | xl | xxl | circle | pill</span>
-                <span className="badge text-bg-secondary ms-1">boolean</span>
-              </>
+              <Link to="#badgeComponentTypes">
+                <span className="badge text-bg-secondary d-inline">
+                  Rounded
+                  <i className="bi bi-link ms-1"></i>
+                </span>
+                <span className="badge text-bg-secondary d-inline ms-1">boolean</span>
+              </Link>
             ),
           },
         ]}
         props
         state={state}
         t={tBadgeComponentProps}
+      />
+
+      <TypesIndicator />
+
+      <Example
+        hash="badgeComponentProps"
+        items={[
+          {
+            attr: 'Variant',
+            default: '',
+            desc: tBadgeComponentProps('badge.desc.variant'),
+            type: (
+              <div className="row row-cols-auto g-1">
+                <div className="col">
+                  <span className="badge text-bg-secondary">secondary</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">primary</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">success</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">warning</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">danger</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">light</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">dark</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">info</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">link</span>
+                </div>
+              </div>
+            ),
+          },
+          {
+            attr: 'Rounded',
+            default: '',
+            desc: tBadgeComponentProps('badge.desc.rounded'),
+            type: (
+              <div className="row row-cols-auto g-1">
+                <div className="col">
+                  <span className="badge text-bg-secondary">circle</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">pill</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">xxl</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">lg</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">md</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">sm</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">xl</span>
+                </div>
+                <div className="col">
+                  <span className="badge text-bg-secondary">xs</span>
+                </div>
+              </div>
+            ),
+          },
+        ]}
+        state={state}
+        t={tBadgeComponentProps}
+        types
       />
 
       <Example hash="commonComponentProps" props state={state} />
